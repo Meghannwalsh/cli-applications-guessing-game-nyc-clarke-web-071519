6d8a7833110b9a84727dc21901f2ac10a836b22a
Meghann McCool Walsh <meghann.mccool.walsh@gmail.com>
2019-07-13 15:32:51 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,8 +1,8 @@
 # Code your solution here!
 
 
-def run_guessing_game(answer)
-  #answer = rand(1..6).to_s
+def run_guessing_game
+  answer = rand(1..6).to_s
   #puts "put a number between 1 and 6"
   guess = gets.chomp.to_s
   if guess == 'exit'
