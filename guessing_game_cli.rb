a114b93aad7e2b0d14cba4050388290b8722da36
Meghann McCool Walsh <meghann.mccool.walsh@gmail.com>
2019-07-13 15:18:55 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,9 +5,9 @@ def run_guessing_game
   answer = rand(1..6)
   puts "put a number between 1 and 6"
   guess = gets.chomp.to_i
-  if guess == answer
+  if guess != answer
     puts "Sorry! The computer guessed #{guess}"
-  elsif guess != answer
+  elsif guess == answer
     puts "You guessed the correct number!"
   elsif guess == 'exit'
     puts "/Goodbye!/"
