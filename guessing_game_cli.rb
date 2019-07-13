e2211facddab9518c702f3b926ba600876733183
Meghann McCool Walsh <meghann.mccool.walsh@gmail.com>
2019-07-13 15:17:01 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -9,5 +9,7 @@ def run_guessing_game
     puts "Sorry! The computer guessed #{guess}"
   elsif guess != answer
     puts "You guessed the correct number!"
+  elsif guess == 'exit'
+    puts "Goodbye!"
   end
 end
