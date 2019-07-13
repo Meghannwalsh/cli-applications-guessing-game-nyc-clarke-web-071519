774ecc29bc3edb8cb9919eda3eda8710f6e5fe7f
Meghann McCool Walsh <meghann.mccool.walsh@gmail.com>
2019-07-13 15:09:40 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,5 +1,13 @@
 # Code your solution here!
 def guessing_CLI
   answer = gets.chomp
-  
+  if answer == false
+    puts "Sorry! The computer guessed 6"
+end
+
+def run_guessing_game(guess)
+  if guess == false
+    puts "Sorry! The computer guessed #{guess}"
+  else
+    puts "You guessed the correct number!"
 end
