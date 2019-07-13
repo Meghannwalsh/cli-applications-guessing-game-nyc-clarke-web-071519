9ed9d063fce1466758d12cd3c30c6fa2eca957e2
Meghann McCool Walsh <meghann.mccool.walsh@gmail.com>
2019-07-13 15:30:07 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,9 +7,9 @@ def run_guessing_game
   guess = gets.chomp.to_s
   if guess == 'exit'
     puts "/Goodbye!/"
-  elsif guess != answer
-    puts "Sorry! The computer guessed #{answer}."
   elsif guess == answer
     puts "/You guessed the correct number!/"
+  else
+    puts "Sorry! The computer guessed #{answer}."
   end
 end
